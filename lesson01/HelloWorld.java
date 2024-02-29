2f7cd4870bf585a4c4841a7fdd00842be08ab8d7
Raza Chandiwala <rachandiwala@gmail.com>
2024-02-28 22:15:29 -0500

    My First commit

---
+++ lesson01/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.neoteck.lesson01;
+
+public class HelloWorld {
+	
+	//This is how you write comments
+	//save changes: CTRL +S
+	
+	
+	//This is the main method to write a code:
+	
+	public static void main (String[] args)
+	
+	{
+		//this main is a very special method
+		
+		
+		//shortcut for printLn:
+		//sysout then command + Space Bar for Mac
+		System.out.println ("Hello World!");
+		
+		
+		
+	}
+
+}
---
+++ lesson01/TestClass.java
@@ -0,0 +1,19 @@
+package com.neoteck.lesson01;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//How do I print on the Console
+		System.out.println("Today is Tuesday");
+		System.out.println("This is the end of the first ever Java Clas!");
+		System.out.println("     See      you      tomorrow");
+		
+		
+		//CTRL +SHIFT+F to reformate/edit the code
+	}
+
+}
+
